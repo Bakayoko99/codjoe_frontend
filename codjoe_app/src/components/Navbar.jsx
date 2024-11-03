08655bfe89b9ce7e2fcfc685a6dcc49ebc81a0f5
Bakayoko99 <user email>
2024-11-03 17:32:27 +0100

    searchbar-update

--- codjoe_app/src/components/Navbar.jsx
+++ codjoe_app/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 // import { styles } from '../styles.js';
@@ -23,8 +23,11 @@ const Navbar = () => {
     const [toggle, setToggle] = useState(false);
     const [hideAlert, setHideAlert] = useState(false);
     const [openSearch, setOpenSearch] = useState(false);
+    const [searchData, setSearchData] = useState(['', []]);
 
-    const { isLoggedIn, setIsLoggedIn, isSignup, loginSuccess, userID, userCart, user } = useCodjoeData();
+    const searchRef = useRef(null)
+
+    const { isLoggedIn, setIsLoggedIn, isSignup, loginSuccess, userID, userCart, user, products } = useCodjoeData();
     // const { ref, isComponentVisible } = useComponentVisible(false);
 
     // const handleCart = () => (
@@ -36,6 +39,40 @@ const Navbar = () => {
     //     setUser(userID)
     //     console.log('userID ',userID);
     // }, [])
+
+    const testProductss = [
+        'black shirt',
+        'white shirt',
+        'yellow shirt',
+        'blue shirt',
+    ]
+
+    const handleSearch = (e) => {
+        // e.preventDefault()
+
+        let result = products.filter(product => product.name.toLowerCase().includes(e))
+        // let result = testProductss.filter(name => name.includes(e))
+        setSearchData([e, result])
+
+    }
+
+
+    const inputBlur = (e) => {
+
+        setTimeout(() => {
+            e.target.value = ''
+            setSearchData([e.target.value, []])
+            // console.log('inputtt bluuuurrr', e.target.value);
+            setOpenSearch(false)
+        }, 200);
+    }
+
+    const inputFocus = () => {
+
+        setOpenSearch(true)
+        searchRef.current.focus()
+
+    }
 
 
     useEffect(() => {
@@ -52,9 +89,6 @@ const Navbar = () => {
         navigate('/')
         window.location.reload()
     }
-
-
-
 
     return (
         <>
@@ -75,14 +109,62 @@ const Navbar = () => {
 
                     </Link>
 
-                    <ul className="flex gap-5">
+                    <ul className="flex gap-5 items-center">
                         <li>
-                            <div className='flex cursor-pointer bg-[#fff] rounded-3xl items-center ' onMouseOver={() => setOpenSearch(true)} onMouseOut={() => setOpenSearch(false)}>
-                                {/* <div className=' w-6'> */}
-                                <input className={`border-0 outline-0 shadow-lg rounded-3xl text-black bg-transparent duration-500 ${openSearch ? 'min-[740px]:w-96 max-[560px]:w-20 w-60' : 'w-0'}`} type="text" placeholder='Search...' />
-                                <img src={search} alt="search" />
+                            {/* <div className={`flex cursor-pointer bg-[#fff] rounded-3xl items-center`} onMouseOver={() => setOpenSearch(true)} onClick={() => setOpenSearch(true)}> */}
+                            <div className={`flex bg-[#fff] rounded-3xl flex-col`} onMouseOver={() => inputFocus()}>
+                                {/* <div className={` w-6 h-10 flex justify-end rounded-3xl duration-1000 shadow-slate-600 drop-shadow-2xl shadow-2xl border-2 ${openSearch ? 'min-[740px]:w-[375px] max-[560px]:w-20 w-60 px-5' : 'w-6'}`}> */}
+                                <div className={`w-6 h-8 flex justify-end items-center rounded-3xl duration-1000 shadow-black drop-shadow-2xl shadow-2xl ${openSearch ? 'min-[740px]:w-[375px] max-[560px]:w-20 w-60 px-5' : 'w-6'}`}>
+                                    {/* <input className={`border-0 outline-0 rounded-3xl text-black bg-transparent duration-500`} type="text" placeholder='Search...' /> */}
+                                    <form>
+                                        <input
+                                            className={`border-0 outline-0 rounded-3xl text-black bg-transparent duration-1000 ${openSearch ? 'min-[740px]:w-[312px] max-[560px]:w-20 w-60' : 'w-0'}`}
+                                            ref={searchRef}
+                                            name='search'
+                                            type="text"
+                                            placeholder='Search...'
+                                            onChange={(e) => handleSearch(e.target.value)}
+                                            onBlur={(e) => inputBlur(e)}
+                                        />
+                                    </form>
+                                    <img className='w-6 h-6 cursor-pointer' src={search} alt="search" />
 
-                                {/* </div> */}
+                                </div>
+                                <div className={`bg-white/20 rounded-lg shadow-lg backdrop-blur-sm border border-white/30 ${searchData[0].length > 1 ? 'min-h-16' : 'h-0'} w-[336px] mt-8 absolute rounded-b-2xl`}>
+                                    {
+                                        searchData[0].length > 1 && searchData[1].length == 0 &&
+                                        <div className='bg-slate-500 h-16 rounded-b-2xl flex items-center justify-center'>no products found</div>
+                                    }
+                                    {
+                                        searchData[0].length > 1 && searchData[1].length > 0 &&
+                                        <ul className='h-full'>
+                                            {searchData[1].map((el) => (
+                                                <li
+                                                    key={el._id}
+                                                    className='bg-white/65 rounded-lg shadow-lg backdrop-blur-lg border border-white/30 m-3 p-2 cursor-pointer text-black h-10'
+                                                >
+                                                    {/* {h-16} */}
+                                                    {/* <Link to={`/list/tops`}> */}
+                                                    {/* <Link to={`/product/${el._id}`}>
+                                                        {el.name}
+                                                    </Link> */}
+                                                    <a href={`/product/${el._id}`}>
+                                                        {el.name}
+                                                    </a>
+                                                    {/* <div className='h-full'>
+                                                            <div className='h-8 w-8'>
+                                                                <img src={el.mainImg} alt={el.name} className='object-cover' />
+                                                            </div>
+                                                            <div>
+                                                                <p>{el.name}</p>
+                                                            </div>
+                                                        </div> */}
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    }
+                                </div>
+
                                 {/* <Link
                                     to="/"
                                     className="text-sm text-center flex text-gray-600 hover:text-gray-800"
